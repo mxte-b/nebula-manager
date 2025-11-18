6ba3a76ac634cd98cbdd5446f485f0a4d1de82ef
mxte-b <182339124+mxte-b@users.noreply.github.com>
2025-11-18 16:33:40 +0100

    Overlay fixes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,16 @@
-
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 
 #[tauri::command]
 async fn open_overlay(app: tauri::AppHandle) {
-    tauri::WebviewWindowBuilder::new(&app, "overlay", tauri::WebviewUrl::App("overlay/index.html".into()))
+    tauri::WebviewWindowBuilder::new(
+        &app,
+        "overlay",
+        tauri::WebviewUrl::App("overlay/index.html".into()),
+    )
     .title("Nebula Manager Overlay")
     .decorations(false)
     .always_on_top(true)
+    .center()
     .build()
     .unwrap();
 }
@@ -14,6 +18,7 @@ async fn open_overlay(app: tauri::AppHandle) {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![open_overlay])
         .run(tauri::generate_context!())
--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,4 +1,25 @@
+import { register } from "@tauri-apps/plugin-global-shortcut";
+import { useEffect } from "react";
+
+await register("CommandOrControl+K", (e) => {
+    // Ignore key release
+    if (e.state == "Released") return;
+
+    console.log("Opening overlay");
+});
+
 const Overlay = () => {
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key == "Escape") {
+            console.log("Leaving overlay");
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+    });
+
     return (
         <div className="overlay">Overlay</div>
     )
